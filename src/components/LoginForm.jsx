0cd7765f4ffba41c943c03f86a15a8330a4c36bf
Dhruv Shah <dhruvshahrds@gmail.com>
2023-06-04 06:14:00 +0530

    add linkedin redirect

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,14 +4,13 @@ const LoginForm = () => {
 
   const linkedInLogin = async () => {
     const scope = ["r_liteprofile", "r_emailaddress", "w_member_social"]
-    const URL = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864siauvsuqfcz&redirect_uri=http://localhost:8000&scope=${scope.join(" "  )}`
+    const URL = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864siauvsuqfcz&redirect_uri=http://localhost:8000/api/users/linkedin/callback/&scope=${scope.join(" "  )}`
     window.location.href = URL;
   }
 
   return (
     <div className="m-10">
 
-      <Link to="/app">
       <button
         style={{
           background:
@@ -19,11 +18,10 @@ const LoginForm = () => {
           border: "1px solid #97AAB1",
         }}
         className="rounded-lg mt-8 text-2xl w-[562px] h-[45px] text-white"
-        // onClick={linkedInLogin}
+        onClick={linkedInLogin}
       >
         Log In with LinkedIn
       </button>
-      </Link>
     </div>
   );
 };
